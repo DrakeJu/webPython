fb077038e1de8d09d7b045392cc1de2c22b01c84
Drake, Ju <zudrake.yh@gmail.com>
2018-08-24 02:26:25 -0700

    2nd

---
+++ syntax/conditional.py
@@ -0,0 +1,26 @@
+user_id = input ('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '1111':
+    print('hello master')
+else:
+    print('who r u?')
+'''
+'''
+if user_id =='drake'
+
+    if user_pwd == '1111':
+        print('hello master')
+    else:
+        print('who r u?')
+else:
+    print('who are you?')
+'''
+
+if user_id =='drake' and user_pwd == '1111':
+    print('hello master')
+elif if user_id =='crystal' and user_pwd == '2222':
+    print('hello master')
+else:
+    print('who are you?')
